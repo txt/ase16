08f7f5c324bc86c125fdc8c89d15f1f3283b1fd9
Tim Menzies <tim@menzies.us>
2016-08-18 00:39:37 -0400

    saving

---
+++ src/package101/__init__.py
@@ -0,0 +1,9 @@
+import sys
+sys.dont_write_bytecode = True
+
+import file1
+import file2
+
+def main():
+  file1.hello()
+  file2.hello()
--- src/package101/__main__.py
+++ src/package101/__main__.py
@@ -1,11 +1,6 @@
 import sys
 sys.dont_write_bytecode = True
 
-import file1
-import file2
-
-def main():
-  file1.hello()
-  file2.hello()
+from __init__ import *
 
 main()
