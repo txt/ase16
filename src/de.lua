5ec4e403a9cf72b489b07e528ce657f0783097a1
Tim Menzies <tim.menzies@gmail.com>
2016-09-30 23:34:59 -0400

    saving

--- src/de.lua
+++ src/de.lua
@@ -24,6 +24,16 @@ end
 
 r = math.random
 
+function any(t) return t[ r(#t) ] end
+
+local function has(t, x)
+  for _,y in pairs(t) do
+    if y == x then 
+      return true
+  end end
+  return false
+end
+
 --------------------------------------
 local de={}
 
@@ -36,20 +46,19 @@ function de.num(t)
   return t
 end
 
-function de.dec(t) 
-  de.num(t)
-  return t
+function de.sym(t)
+  t.all = t.all or {}
 end
 
 function de.obj(t)  
   de.num(t)
   t.better = t.better or less
-  t.get  = t.get or de.numAny
+  t.get    = t.get or de.numAny
   return t
 end
 
 function de.xy(t)
-  t= t or {}
+  t   = t or {}
   t.x = t.x or {}
   t.y = t.y or {}
   return t
@@ -57,24 +66,42 @@ end
 -------------------------------
 function de.model(t)
   de.xy(t)
-  t.ok   = t.ok   or (function (_) return True)
+  t.ok = t.ok   or (function (_) return True end)
 end
 
-function de.numAny(n)
-  return n.lo + r()*(n.hi - n.lo)
+function de.ok(i, t, wants)
+  local function num()
+    assert t[i] >= wants[i].lo and t[i] <= wants[j].hi end
+  local function sym()
+    assert has(wants[i].all, t[i]) end
+   if t[i] ~= nil then
+     f = (t.all and num or sym)()
+end end 
+
+function de.oks(wants,t)
+  for i = 1,#wants.x do de.ok(i, t.x, wants.x) end 
+  for i = 1,#wants.y do de.ok(i, t.y, wants.y) end
 end
 
-function de.model2decs(want)
+function de.any(want)
+  local function num() return want.lo + r()*(want.hi - want.lo) end
+  local function sym() return any(want.all) end
+  return (want.all and sym or num)()
+end
+
+function de.model2decs(wants)
   local got = de.xy()
-  for x,v in pairs(want.x) do
-    got.x[k] = de.numAny(v)
+  for k,want in pairs(wants.x) do
+    got.x[k] = de.any(want)
   end
   return got
 end
 
-function de.decs2objs(t)
+function de.decs2objs(wamt,t)
   if t.objs then return t end
-  
+  for y,v in pairs(want.y) do got.x[y] = de.numAny(v) end
+  return t
+end
 
 function de.evolve(m)
   local fronter={}
@@ -100,11 +127,12 @@ function m1()
   function get1(xy)
     return (xy[1] + xy[2])^2
   end
-  local d,o = de.dec,de.obj
+  local s,n,o = de.sym, de.num,de.obj
   return de.model{
     decs={
-      d{name="d1",lo=10,hi=40},
-      d{name="d2",lo=30,hi=50}
+      s{name="day", all={"mon","tue","wed","thr","fri","sat","sun"}}
+      d{name="d1", lo=10, hi=40},
+      d{name="d2", lo=30, hi=50}
     } ,
     objs={
       o{name="o1",get = get1},
